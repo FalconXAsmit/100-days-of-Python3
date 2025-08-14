6f7438259872950dd0818c2d4d6f4d039304236a
Asmit <kumarasmit2007@gmail.com>
2025-08-14 18:00:48 +0530

    Complete Day 1 - Printing, input, and String Manipulation

--- Learning/Day_001.py
+++ Learning/Day_001.py
@@ -12,11 +12,11 @@ print("5. Bake at 200 degrees C for 30 minutes.")
 # # This line prints "Hello world!" twice, each on a new line. where \n is a newline character.
 print("Hello world!\nHello world!\nHello world!")
 # This line concatenates the strings "Hello" and "FalconX" to form "HelloFalconX". There is no space between the two strings.
-print("Hello"+"FalconX")
+print("Hello" + "FalconX")
 # This line concatenates the strings "Hello", " ", and "FalconX" to form "Hello FalconX". The space is added between "Hello" and "FalconX".
-print("Hello"+" "+"FalconX")
+print("Hello" + " " + "FalconX")
 # This line concatenates the string "Hello " (with a space at the end) and "FalconX" to form "Hello FalconX". The space is included in the first string.
-print("Hello "+"FalconX")
+print("Hello " + "FalconX")
 
 # Coding Practice 2(Debugging Practice)
 # print(Notes from Day 1")
@@ -24,19 +24,23 @@ print("Hello "+"FalconX")
 # print("Strings are strings of characters"
 # priint("String Concatenation is done with the + sign")
 # print(("New lines can be created with a \ and the letter n")
-print("Notes from Day 1") # " marks at the start missing
-print("The print statement is used to output strings") # Unnecessary space at the start of the line
-print("Strings are strings of characters") # Missing closing paranthesis at the end of the string
+print("Notes from Day 1")  # " marks at the start missing
+print(
+    "The print statement is used to output strings"
+)  # Unnecessary space at the start of the line
+print(
+    "Strings are strings of characters"
+)  # Missing closing paranthesis at the end of the string
 print("String Concatenation is done with the + sign")
 print("New lines can be created with a \ and the letter n")
 
 # Input function
 # This line prompts the user for their name and then prints "Hello " followed by the user's name and the exclamation mark.
-print("Hello " + input("What is your name?: ") + "!") 
+print("Hello " + input("What is your name?: ") + "!")
 
 # Variables
 # This line stores the user's input in the variable 'name'.
-name = input("What is your name?: ") 
+name = input("What is your name?: ")
 # This line prints the value stored in the variable 'name'.
 print(name)
 # This line prints the length of the string stored in 'name'.
@@ -47,7 +51,7 @@ username = input("What is your name?: ")
 length = len(username)
 print(length)
 
-#Variable Naming Conventions
+# Variable Naming Conventions
 # 1. Make sure the variable name is descriptive and meaningful.
 # 2. Don't have space in between.
 # 3. Don't start with a number.
