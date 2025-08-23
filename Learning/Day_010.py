6020acdbbce245745ea23aac9af08962ea9b6fd8
Asmit <kumarasmit2007@gmail.com>
2025-08-23 23:27:15 +0530

    Day 10 - Functions With Outputs

---
+++ Learning/Day_010.py
@@ -0,0 +1,42 @@
+# Functions with Outputs
+def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid inputs."
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    return f"Result: {formated_f_name} {formated_l_name}"
+
+#Storing output in a variable
+formatted_name = format_name(input("Your first name: "), input("Your last name: "))
+print(formatted_name)
+#or printing output directly
+print(format_name(input("What is your first name? "), input("What is your last name? ")))
+
+# Coding Exercise 10
+def is_leap_year(year):
+    # Write your code here. 
+    # Don't change the function name.
+    leap = False
+    if year%4 == 0:
+        leap = True
+    
+    if year%100 == 0:
+        if year%400 == 0:
+            pass
+        else:
+            leap = False
+    return leap
+
+is_leap_year(2400)
+
+#Return as an early exit
+def format_name(f_name, l_name):
+  """Take a first and last name and format it 
+  to return the title case version of the name."""
+  if f_name == "" or l_name == "":
+    return "You didn't provide valid inputs."
+  formated_f_name = f_name.title()
+  formated_l_name = l_name.title()
+  return f"Result: {formated_f_name} {formated_l_name}"
+
+output = format_name("AsMIt", "KuMar")
