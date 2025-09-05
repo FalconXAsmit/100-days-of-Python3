d3ac2128aec25e5729994b749743b5652736ff6d
Asmit <kumarasmit2007@gmail.com>
2025-09-06 00:36:11 +0530

    Day 22 Project Pong Game

--- Projects/Day 022/main.py
+++ Projects/Day 022/main.py
@@ -27,11 +27,9 @@ while game_is_on:
     screen.update()
     ball.move()
 
-    # Detect collision with wall
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
 
-    # Detect collision with paddle
     if (
         ball.distance(r_paddle) < 50
         and ball.xcor() > 320
@@ -40,12 +38,10 @@ while game_is_on:
     ):
         ball.bounce_x()
 
-    # Detect R paddle misses
     if ball.xcor() > 380:
         ball.reset_position()
         scoreboard.l_point()
 
-    # Detect L paddle misses:
     if ball.xcor() < -380:
         ball.reset_position()
         scoreboard.r_point()
