31b9cd9f8d9f4199daeb42e44fe964d2f6b3e472
Asmit <kumarasmit2007@gmail.com>
2025-09-05 14:33:23 +0530

    Day 20 & 21 Project Snake Game Complete

---
+++ Learning/Day_021.py
@@ -0,0 +1,20 @@
+# Class Inheritance
+
+class Animal:
+    def __init__(self):
+        self.num_eyes = 2
+    
+    def breath():
+        print("Inhale, Exhale")
+    
+class Fish(Animal):
+
+    def __init__(self):
+        super().__init__()
+
+    def swim(self):
+        print("Moving in water")
+
+nemo = Fish()
+
+nemo.swim()
--- Projects/Day 020-021/snake.py
+++ Projects/Day 020-021/snake.py
@@ -16,11 +16,8 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITION:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segment.append(new_segment)
+            self.add_segment(position)
+            
 
     def move(self):
         for seg_num in range(len(self.segment) - 1, 0, -1):
@@ -44,3 +41,14 @@ class Snake:
     def right(self):
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
+    
+    def add_segment(self,position):
+        new_segment = Turtle("circle")
+        new_segment.color("blue")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segment.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segment[-1].position())
+
