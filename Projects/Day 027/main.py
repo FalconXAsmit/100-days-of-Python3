1e8d18ae4b513c0de00384568db30e6575efc6e6
Asmit <kumarasmit2007@gmail.com>
2025-09-14 17:51:21 +0530

    Day 27 Project Miles to Kilometer Converter GUI

---
+++ Projects/Day 027/main.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+
+def miles_to_km():
+    miles = float(miles_input.get())
+    km = miles * 1.609
+    kilometer_result_label.config(text=f"{km}")
+
+window = tk.Tk()
+window.title("Mile to KM converter")
+window.config(padx=50, pady=50)
+
+miles_input = tk.Entry(width=7)
+miles_input.grid(column=1, row=0)
+
+miles_label = tk.Label(text= "Miles")
+miles_label.grid(column=2, row=0)
+
+is_equal_label = tk.Label(text="is equal to")
+is_equal_label.grid(column=0, row=1)
+
+kilometer_result_label = tk.Label(text="0")
+kilometer_result_label.grid(column=1, row=1)
+
+kilometer_label = tk.Label(text="km")
+kilometer_label.grid(column=2, row=1)
+
+calculate_button = tk.Button(text="Calculate", command=miles_to_km)
+calculate_button.grid(column=1, row=2)
+
+
+
+window.mainloop()
